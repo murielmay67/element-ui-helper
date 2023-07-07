51e67b4e327c16cfe5564c9529e85efa368250dc
严浩 <me@yanhao.ren>
2023-07-07 16:34:39 +0800

    vue2Jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import vue2 from '@vitejs/plugin-vue2'
+import vue2Jsx from '@vitejs/plugin-vue2-jsx'
 
-const path = require('path')
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [vue2(), vue2Jsx()],
+  build: {
+    sourcemap: true,
+    minify: false,
+  },
   resolve: {
     alias: [
       {
         find: /^@\/(.+)/,
-        replacement: path.resolve(path.resolve(__dirname), 'src') + '/$1'
-      }
-    ]
-  }
+        replacement: path.resolve(path.resolve(__dirname), 'src') + '/$1',
+      },
+    ],
+  },
 })
