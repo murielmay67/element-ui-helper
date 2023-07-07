3451e3d12a7c68ffa241a4c78d977f9b43b6cf8c
严浩 <me@yanhao.ren>
2023-07-07 17:58:13 +0800

    Revert "split-off! vue-frag"
    This reverts commit bdbadeeb9e299adbee77e0c8dbba11a6e676d48b.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue2 from '@vitejs/plugin-vue2'
 import vue2Jsx from '@vitejs/plugin-vue2-jsx'
-import { vueFrag } from 'vue-frag-plugin'
 
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vueFrag(), vue2(), vue2Jsx()],
+  plugins: [vue2(), vue2Jsx()],
   build: {
     sourcemap: true,
     minify: false,
